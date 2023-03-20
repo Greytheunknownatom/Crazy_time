8d327952a793d698138b8806b681faab20129ebd
Greytheunknownatom <thenoskshade@gmail.com>
2023-03-20 23:07:55 +0000

    learning

---
+++ myself/speedup.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - traing myself
+ * by thenoskshade
+ * Return: 0
+*/
+int main()
+{
+	printf("hello,grey\nyou become thenoskshade\n");
+	printf("just a man\ndream walking\n");
+	printf("shading the sky\nand his path");
+	printf("\nyo \"am just tryna ride on this shit\"");
+        
+	return (0);
+}
