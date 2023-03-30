538ab71ce4744bc00002bde1e3f0b8dc462e0fbf
Greytheunknownatom <thenoskshade@gmail.com>
2023-03-30 04:09:19 +0000

    learning

---
+++ training/list.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * list - not a function just a list of certain things
+ * thenoskshade-dreamwalking
+ * Return: 0
+*/
+int main(void)
+{
+	int numofbooks = 17;
+	char favletter = 't';
+	char favdigit = '1';
+	double costofpringles = 1700;
+
+	printf("Number of books: %d\n", numofbooks);
+	printf("My favorite letter is: %c\n", favletter);
+	printf("My favorite digits: %c\n", favdigit);
+	printf("I went to the mall and paid $%.2f for a can of pringles", costofpringles);
+	return (0);
+}
