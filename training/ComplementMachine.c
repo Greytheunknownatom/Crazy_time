63a8f24e9e93425f4270eaf08b0569f8f1ade207
Greytheunknownatom <thenoskshade@gmail.com>
2023-03-31 07:54:49 +0000

    learing data types and functions

--- training/ComplementMachine.c
+++ training/ComplementMachine.c
@@ -7,26 +7,26 @@
 int main(void)
 {
 	/* declearation of char and prompt to input name */
-	char name[10];
+	char name[40];
 	printf("welcome master enter your name: ");
 	scanf ("%s", name);
 
 	printf("Hello %s!\nyou are the master of C programming\n", name); 
 
 	char age[20];
-	printf("noskshade please enter your age: ");
+	printf("please enter your age: ");
 	scanf ("%s", age);
 
 	printf("You are %s\nwow so young!\n", age);
 
 	char sistersname[10];
-	printf("Now enter sister's name: ");
+	printf("Now enter your sister's name: ");
 	scanf ("%s", sistersname);
 
 	printf("Your sister's named %s such a beautiful name\n", sistersname);
 
 	char ronniesage[17];
-	printf("enter your age: ");
+	printf("enter your sister's age: ");
 	scanf ("%s", ronniesage);
 
 	printf("sister's age is %s\nsuch a baby!\n", ronniesage);
---
+++ training/comments.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+// comments: here we will be working with comments (single line commment)
+/* (multiline comments) this comment types can house comments beyond just one line */
+/**
+ * main - talking on comments (betty multi line comment smaple)
+ * must contain comment's main function
+ * Return: 0 must contain return value too 
+*/
+int main(void)
+{
+	printf("The classes of food for the day: "/* more information may be added later on */);
+	int classes;
+	scanf("%i", &classes);
+
+	double rooms = (double) rooms / 12;
+
+	printf("you have %f rooms classes.\n", rooms);
+
+	return (0);
+
