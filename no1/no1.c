6a3f886b51aa61deead8a3ee2f6bf8a5695a68a1
Greytheunknownatom <thenoskshade@gmail.com>
2023-04-23 14:34:07 +0000

    fixing

---
+++ no1/no1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(void)
+{	
+	printf("i like flowers!\n");
+	printf("i really like flowers\n");
+	printf("this is my contact number below\n0\t9\t0\n5\t9\t5\n4\t7\t9\n0\t6\n");
+	printf("\"i really like to code yah know\"\n");
+	printf("\\comments look like this\\\n");
+	
+//introducing variables
+	
+	int r; //declaration
+	r = 123; //initialization
+	int i = 123; //declaration & initialization
+	
+	int age = 21; //integer
+	float gpa = 3.05; //floating point number
+	char grade = 'C'; //single character
+	char name[] = "Grey"; //array of characters
+	
+	printf("look at %s he is aged %i/nwith a gpa score of %f and overall grade of %c", name, age, gpa
+);
+return 0;
+}
